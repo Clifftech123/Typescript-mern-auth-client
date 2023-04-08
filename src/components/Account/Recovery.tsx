108c30404474e399dfac41f9448538bccf6e2aae
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-04-08 21:27:21 +0000

    Adding of recovery component

--- src/components/Account/Recovery.tsx
+++ src/components/Account/Recovery.tsx
@@ -1,11 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../../assets/profile.png";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { passwordValidate } from "../../helper/validate";
 
+import styles from "../../styles/Account/Logging.module.css";
 
-
- const Recovery = () => {
+const Recovery = () => {
   return (
-    <div className='text-center'>Recovery</div>
-  )
-}
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
 
+      <div className="flex justify-center   items-center h-screen">
+        {/*   This section contain all the form content      */}
+        <div className={styles.formContainer}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Recovery </h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Enter OTP to recovery your password
+            </span>
+          </div>
+          {/* ============== FORM ==================  */}
+          <form className="pt-20">
+            {/* SUBMIT  BUTTON : All the styling for this button  found in the module logging css   */}
+            <div className="textbox flex flex-col items-center gap-6">
+              <div className="input text-center">
+                <span className="py-4  text-sm text-left text-gray-500 ">
+                  Enter 6 digit OTP sent to your email address
+                </span>
+              </div>
+              <input
+                className={styles.textbox}
+                type="password"
+                placeholder="OTP"
+              />
 
-export default Recovery
+              <button className={styles.btn}> Recovery </button>
+            </div>
+            {/*  REGISTER*/}
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Can't get OTP? <button className="text-red-500">Resend</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recovery;
